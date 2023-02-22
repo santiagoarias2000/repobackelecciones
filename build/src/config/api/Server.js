fe8451cdfaf4d8947afe83b18e8a6c96fdf27a9d
santiagoarias2000 <santiago.ariasm@gmail.com>
2023-02-21 21:35:12 -0500

    funciona con el puesto 3123 y protocolo http

--- build/src/config/api/Server.js
+++ build/src/config/api/Server.js
@@ -20,7 +20,7 @@ class Server {
         this.activeRoute();
     }
     startSetting() {
-        this.app.set("PORT", 443);
+        this.app.set("PORT", 3123);
         this.app.use((0, cors_1.default)());
         this.app.use((0, morgan_1.default)("dev"));
         this.app.use(express_1.default.json({ limit: "100mb" }));
